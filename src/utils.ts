b9bd0878c65ce5beda2d3b9b309cbd6a857641f4
Vlad <Smirnov.ru92@mail.ru>
2023-10-24 13:29:42 +0500

    update update data validation

--- src/utils.ts
+++ src/utils.ts
@@ -14,12 +14,12 @@ export const createErrorObject = (message: string, field: string) => {
 
 }
 
-const checkVideoQuality = (videoQuality:Array<string> | null | undefined) => {
+const checkVideoQuality = (videoQuality: Array<string> | null | undefined) => {
     const allowableVideoResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160']
     let res = true
     let incorrectValue = null
-    if(videoQuality === null || videoQuality === undefined) return true
-    if(!Array.isArray(videoQuality) || videoQuality.length < 1) return false
+    if (videoQuality === null || videoQuality === undefined) return true
+    if (!Array.isArray(videoQuality) || videoQuality.length < 1) return false
 
     for (let i = 0; i < videoQuality.length; i++) {
         let elem = videoQuality[i]
@@ -36,49 +36,57 @@ const checkVideoQuality = (videoQuality:Array<string> | null | undefined) => {
 export const validateCreateVideoData = ({author, title, availableResolutions}: VideoCreateModel) => {
     debugger
     if (author.length > 40 || !author || typeof author != 'string') {
-        if(!author || typeof author != 'string') return createErrorObject('no valid filed', 'author')
+        if (!author || typeof author != 'string') return createErrorObject('no valid filed', 'author')
         return createErrorObject(`${author.length > 20 ? "length more then 20" : "Too short name"}`, 'author')
     } else if (!title || title.length > 40 || typeof title != 'string') {
-        if(!title || typeof title != 'string' ) return createErrorObject('no valid filed', 'title')
+        if (!title || typeof title != 'string') return createErrorObject('no valid filed', 'title')
         return createErrorObject(`${title.length > 40 ? "length more then 40" : "Too short name"}`, 'title')
     }
-if(availableResolutions){}
+    if (availableResolutions) {
+    }
 
-    if(!checkVideoQuality(availableResolutions)){
+    if (!checkVideoQuality(availableResolutions)) {
         return createErrorObject(`incorrect quality video`, 'availableResolutions')
     }
 }
 
-export const validateUpdateVideoData = ( {author, title, availableResolutions,minAgeRestriction,canBeDownloaded,publicationDate}: VideoUpdateModel) => {
-    if (author.length > 20 || !author) {
-        return createErrorObject(`${author.length > 40 ? "length more then 40" : "Too short name"}`, 'author')
-    } else if (title.length > 40 || !title) {
+export const validateUpdateVideoData = ({
+                                            author,
+                                            title,
+                                            availableResolutions,
+                                            minAgeRestriction,
+                                            canBeDownloaded,
+                                            publicationDate
+                                        }: VideoUpdateModel) => {
+    if (author.length > 40 || !author || typeof author != 'string') {
+        if (!author || typeof author != 'string') return createErrorObject('no valid filed', 'author')
+        return createErrorObject(`${author.length > 20 ? "length more then 20" : "Too short name"}`, 'author')
+    } else if (!title || title.length > 40 || typeof title != 'string') {
+        if (!title || typeof title != 'string') return createErrorObject('no valid filed', 'title')
         return createErrorObject(`${title.length > 40 ? "length more then 40" : "Too short name"}`, 'title')
     }
 
-    if(minAgeRestriction != undefined){
-    if (minAgeRestriction > 18 || minAgeRestriction < 1 || typeof minAgeRestriction != 'number') {
+    if (minAgeRestriction != undefined) {
+        if (minAgeRestriction > 18 || minAgeRestriction < 1 || typeof minAgeRestriction != 'number') {
             return createErrorObject(`invalid quality`, 'minAgeRestriction')
         }
     }
 
-    if(canBeDownloaded != undefined){
+    if (canBeDownloaded != undefined) {
         if (typeof canBeDownloaded != 'boolean') {
             return createErrorObject(`invalid quality`, 'canBeDownloaded')
         }
     }
 
 
-
-    if(publicationDate != undefined){
-        if ( typeof publicationDate != "string") {
+    if (publicationDate != undefined) {
+        if (typeof publicationDate != "string") {
             return createErrorObject(`invalid quality`, 'publicationDate')
         }
     }
 
 
-
-    if(!checkVideoQuality(availableResolutions)){
+    if (!checkVideoQuality(availableResolutions)) {
         return createErrorObject(`incorrect quality video`, 'availableResolution')
     }
 }
