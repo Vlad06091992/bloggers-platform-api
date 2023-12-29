46ae762db0634d156325dc5f084f442e7e84a8c5
Vlad <Smirnov.ru92@mail.ru>
2023-12-29 14:20:18 +0500

    feat: delete endpoint

--- src/features/posts/posts.router.ts
+++ src/features/posts/posts.router.ts
@@ -23,86 +23,6 @@ import { validateErrors } from "../../middlewares/validateErrors";
 import { ResponsePostsModel } from "./model/response-models/responsePostsModel";
 
 
-export const getPostsRouter = () => {
-  const router = express.Router();
-  router.get(
-    "/",
-    async (
-      req: RequestWithQuery<QueryPostModel>,
-      res: Response<ResponsePostsModel>,
-    ) => {
-      let foundedPosts = await postsRepository.findPosts(req.query);
-      res.status(HTTP_STATUSES.OK_200).send(foundedPosts);
-    },
-  );
-
-  router.post(
-    "/",
-    authGuardMiddleware,
-    validateCreatePostDataWithIdParams,
-    validateErrors,
-    async (
-      req: RequestWithBody<PostCreateModel>,
-      res: Response<PostViewModel>,
-    ) => {
-      let newPost = await postsRepository.createPost(req.body);
-      res.status(HTTP_STATUSES.CREATED_201).send(newPost);
-    },
-  );
-
-  router.get(
-    "/:id",
-    async (
-      req: RequestWithParams<URIParamsPostIdModel>,
-      res: Response<PostViewModel | number>,
-    ) => {
-      const post = await postsRepository.getPostById(req.params.id);
-      if (post) {
-        res.send(post);
-      } else {
-        res.send(HTTP_STATUSES.NOT_FOUND_404);
-      }
-    },
-  );
-
-  router.put(
-    "/:id",
-    authGuardMiddleware,
-    validateCreatePostDataWithIdParams,
-    validateErrors,
-    async (
-      req: RequestWithParamsAndBody<URIParamsPostIdModel, PostUpdateModel>,
-      res: Response<PostViewModel | number>,
-    ) => {
-      const isExistsPost = await postsRepository.getPostById(req.params.id);
-      if (!isExistsPost) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-      }
-      await postsRepository.updatePost(req.params.id, req.body);
-      res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-    },
-  );
-
-  router.delete(
-    "/:id",
-    authGuardMiddleware,
-    async (
-      req: RequestWithParams<URIParamsPostIdModel>,
-      res: Response<number>,
-    ) => {
-      const isDeleted = await postsRepository.deletePost(req.params.id);
-      if (isDeleted) {
-        res.send(HTTP_STATUSES.NO_CONTENT_204);
-      } else {
-        res.send(HTTP_STATUSES.NOT_FOUND_404);
-      }
-    },
-  );
-  return router;
-};
-
-
 /**
  * @openapi
  * /posts:
@@ -324,6 +244,22 @@ export const getPostsRouter = () => {
  *         description: Not Found
  */
 
+/**
+ * @swagger
+ * /testing/all-data:
+ *   delete:
+ *     summary: Clear database, delete all data from all tables/collections
+ *     tags:
+ *       - Testing
+ *     parameters:
+ *       - name: No parameters
+ *         in: None
+ *         description: No parameters are required for this operation.
+ *         required: false
+ *     responses:
+ *       '204':
+ *         description: No Content
+ */
 
 
 /**
@@ -464,3 +400,82 @@ export const getPostsRouter = () => {
  */
 
 
+
+export const getPostsRouter = () => {
+  const router = express.Router();
+  router.get(
+    "/",
+    async (
+      req: RequestWithQuery<QueryPostModel>,
+      res: Response<ResponsePostsModel>,
+    ) => {
+      let foundedPosts = await postsRepository.findPosts(req.query);
+      res.status(HTTP_STATUSES.OK_200).send(foundedPosts);
+    },
+  );
+
+  router.post(
+    "/",
+    authGuardMiddleware,
+    validateCreatePostDataWithIdParams,
+    validateErrors,
+    async (
+      req: RequestWithBody<PostCreateModel>,
+      res: Response<PostViewModel>,
+    ) => {
+      let newPost = await postsRepository.createPost(req.body);
+      res.status(HTTP_STATUSES.CREATED_201).send(newPost);
+    },
+  );
+
+  router.get(
+    "/:id",
+    async (
+      req: RequestWithParams<URIParamsPostIdModel>,
+      res: Response<PostViewModel | number>,
+    ) => {
+      const post = await postsRepository.getPostById(req.params.id);
+      if (post) {
+        res.send(post);
+      } else {
+        res.send(HTTP_STATUSES.NOT_FOUND_404);
+      }
+    },
+  );
+
+  router.put(
+    "/:id",
+    authGuardMiddleware,
+    validateCreatePostDataWithIdParams,
+    validateErrors,
+    async (
+      req: RequestWithParamsAndBody<URIParamsPostIdModel, PostUpdateModel>,
+      res: Response<PostViewModel | number>,
+    ) => {
+      const isExistsPost = await postsRepository.getPostById(req.params.id);
+      if (!isExistsPost) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
+      }
+      await postsRepository.updatePost(req.params.id, req.body);
+      res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+    },
+  );
+
+  router.delete(
+    "/:id",
+    authGuardMiddleware,
+    async (
+      req: RequestWithParams<URIParamsPostIdModel>,
+      res: Response<number>,
+    ) => {
+      const isDeleted = await postsRepository.deletePost(req.params.id);
+      if (isDeleted) {
+        res.send(HTTP_STATUSES.NO_CONTENT_204);
+      } else {
+        res.send(HTTP_STATUSES.NOT_FOUND_404);
+      }
+    },
+  );
+  return router;
+};
