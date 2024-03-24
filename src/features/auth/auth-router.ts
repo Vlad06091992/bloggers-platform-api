78741ce9488bbaea69beade4e93ac02c938f17ca
Vlad <Smirnov.ru92@mail.ru>
2024-03-24 11:05:18 +0500

    hw 5 fix validate create user data

--- src/features/auth/auth-router.ts
+++ src/features/auth/auth-router.ts
@@ -7,18 +7,22 @@ import {AuthCreateModel} from "./types/types";
 import {authService} from "../auth/auth-service";
 import {WithId} from "mongodb";
 import {UserType} from "../users/types/types";
+import {validateCreateUserData} from "./validators/validateUserCredentials";
+import {validateErrors} from "../../middlewares/validateErrors";
 
 export const getAuthRouter = () => {
     const router = express.Router();
     router.post(
         "/login",
+        validateCreateUserData,
+        validateErrors,
         async (
             req: RequestWithBody<AuthCreateModel>,
             res: Response<any>,
         ) => {
             let user: WithId<UserType> = await usersService.findUserByLoginOrEmail(req.body.loginOrEmail, "object") as WithId<UserType>;
             if (!user) {
-                res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+                res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
             } else {
                 const result = await authService.checkPassword(user.password, req.body.password)
                 if (!result) {
