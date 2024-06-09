d6a6a8bfb484280a19005138485b3d8be20759a6
Vlad <Smirnov.ru92@mail.ru>
2024-06-09 23:39:26 +0500

    hw 12(mongoDB integrations settings)

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import { app } from "./app";
 import { runDb } from "./db-mongo";
 import {emailAdapter} from "./adapters/email-adapter";
 
-const port = 3000 ;
-app.listen(port, async () => {
-  await runDb();
-  console.log(`Example app listening on port ${port}`);
-});
+const port = 5000 ;
 
+export const start = async () => {
+  try{
+    app.listen(port, async () => {
+      await runDb();
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (e){
+    console.log(e)
+  }
+
+}
+
+start()
 module.exports = app
